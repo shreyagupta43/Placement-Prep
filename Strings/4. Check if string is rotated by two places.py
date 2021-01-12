67e6f30869902562bc97f979d01eb639ec6b2413
shreyagupta43 <shreyagupta4300@gmail.com>
2021-01-13 02:03:24 +0530

    4C

--- Strings/4. Check if string is rotated by two places.py
+++ Strings/4. Check if string is rotated by two places.py
@@ -1,3 +1,4 @@
+#With Space Complexity = O(n)
 def isRotated(s,p):
     n=len(s)
     x=s+s
@@ -5,7 +6,21 @@ def isRotated(s,p):
         if(n==1 or n==2) and (s==p):
             return True
         
-        
         if(p==x[2:n+2]) or (p==x[n-2:n+4]):
             return True
+    return False
+
+
+    
+    #With Space Complexity = O(1)
+    def isRotated(s,p):
+    n = len(s)
+    
+    if (len(s)==len(p)):
+        if (n==1 or n==2) and s==p:
+            return True
+        if n>2:
+            if ((s[2:] == p[:n-2] or s[:n-2] == p[2:])):
+                return True
+
     return False
