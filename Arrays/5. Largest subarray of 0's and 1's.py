64ad643d28df1964b9989fe824e3cec4082c4654
shreyagupta43 <shreyagupta4300@gmail.com>
2021-01-14 23:30:11 +0530

    =

---
+++ Arrays/5. Largest subarray of 0's and 1's.py
@@ -0,0 +1,22 @@
+def maxLen(arr, N):
+    
+    for i in range(N):
+        if arr[i]==0:
+             arr[i] = -1  
+    
+    sum = 0
+    equalNo = 0
+    
+    dict = {}
+    
+    for i in range(N):
+        sum = sum + arr[i]
+        
+        if sum == 0:
+            equalNo = i+1
+        if sum in dic:
+            equalNo = max(i-dict[sum], equalNo)
+        else:
+            dict[sum] = i
+    
+    return equalNo 
