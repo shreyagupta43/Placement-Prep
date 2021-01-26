bb27a5a238009648dc3fbbc07c1127b36613d6df
shreyagupta43 <shreyagupta4300@gmail.com>
2021-01-26 22:27:15 +0530

    c

--- Arrays/3. LeadersArray.py
+++ Arrays/3. LeadersArray.py
@@ -2,8 +2,10 @@ def leaders(A,n):
     lis=[]
     if(n==1):
         return A[0]
+        
     for i in range(n):
         a=0
+
         for j in range(i+1,n):
             if(A[i]>=A[j]):
                 continue
